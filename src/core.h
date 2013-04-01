6a5ed91b961e70306cac7259ccc8b28fe9c9c529
Emanuel Eichhammer <DerManu@WorksLikeClockwork.com>
2013-04-02 01:31:43 +0200

    - Added QCustomPlot::savePs interface for saving Postscript files

--- src/core.h
+++ src/core.h
@@ -147,6 +147,7 @@ public:
   QList<QCPAxis*> selectedAxes() const;
   QList<QCPLegend*> selectedLegends() const;
   bool savePdf(const QString &fileName, bool noCosmeticPen=false, int width=0, int height=0);
+  bool savePs(const QString &fileName, bool noCosmeticPen=false, int width=0, int height=0);
   bool savePng(const QString &fileName, int width=0, int height=0, double scale=1.0, int quality=-1);
   bool saveJpg(const QString &fileName, int width=0, int height=0, double scale=1.0, int quality=-1);
   bool saveBmp(const QString &fileName, int width=0, int height=0, double scale=1.0);
