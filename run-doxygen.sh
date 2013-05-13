26533a29108b317700d6412c06c80060cd6b31ed
Emanuel Eichhammer <DerManu@WorksLikeClockwork.com>
2013-05-14 01:53:25 +0200

    - doc image copying is now done by run-doxygen.sh script again (because Doxyfile's IMAGE_PATH doesn't copy images added as html-img)

--- run-doxygen.sh
+++ run-doxygen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd "$( dirname "$0" )"
-rm -r "doc/html"
+rm -r doc/html
+mkdir doc/html
+
+# copy doc images to doc/html directory. Doxyfile's IMAGE_PATH includes them from there:
+cp other/doc-image-generator/images/* doc/html/
+cp doc/images/* doc/html/
+
 doxygen
 cd doc/html
 rm *.map *.md5 index.qhp
-
-# image copying is done with Doxyfile IMAGE_PATH 
-# cp ../../other/doc-image-generator/images/* ./
-# cp ../*.png ./
 
