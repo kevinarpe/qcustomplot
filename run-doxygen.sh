44bcda069a01f13b54896614749e9458a581b92f
Emanuel Eichhammer <DerManu@WorksLikeClockwork.com>
2013-05-15 02:02:14 +0200

    - Renamed "doc" directory to "documentation"

--- run-doxygen.sh
+++ run-doxygen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd "$( dirname "$0" )"
-rm -r doc/html
-mkdir doc/html
+rm -r documentation/html
+mkdir documentation/html
 
-# copy doc images to doc/html directory. Doxyfile's IMAGE_PATH includes them from there:
-cp other/doc-image-generator/images/* doc/html/
-cp doc/images/* doc/html/
+# copy doc images to documentation/html directory. Doxyfile's IMAGE_PATH includes them from there:
+cp other/doc-image-generator/images/* documentation/html/
+cp doc/images/* documentation/html/
 
 doxygen
-cd doc/html
+cd documentation/html
 rm *.map *.md5 index.qhp
 
