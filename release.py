98c8cf5e09441d3bcfeee893a8b183cb3f23bead
Emanuel Eichhammer <DerManu@WorksLikeClockwork.com>
2013-05-15 02:26:15 +0200

    - Reworked release.py script

--- release.py
+++ release.py
@@ -6,24 +6,6 @@ def printinfo(message):
 
 def printerror(message):
   print "\033[1;31m"+message+"\033[1;m"
-  
-def removeExcludeBlockFromFile(fname):
-  os.rename(fname, fname+".temp")
-  original = open(fname+".temp", "r")
-  new = open(fname, "w")
-  excludeBlock = False;
-  line = original.readline();
-  while line:
-    if "release-exclude-start" in line:
-      excludeBlock = True
-    if not excludeBlock:
-     new.write(line);
-    if "release-exclude-end" in line:
-      excludeBlock = False
-    line = original.readline();
-  original.close()
-  new.close()
-  os.remove(fname+".temp")
 
 tarcommand = "GZIP=\"-9\" tar -caf" # -a means determine compressor by archive suffix
 tarsuffix = ".tar.gz"
@@ -32,58 +14,34 @@ os.chdir(sys.path[0]) # change current working dir to script dir
 os.mkdir("./temp")
 os.chdir("./temp")
 
-# documentation:
-printinfo("Building QCustomPlot-doc package")
-distutils.dir_util.copy_tree("../doc/html", "./html")
-shutil.copy2("../doc/qthelp/qcustomplot.qch", "./")
-subprocess.call(tarcommand+" QCustomPlot-doc"+tarsuffix+" *", shell=True)
-shutil.move("QCustomPlot-doc"+tarsuffix, "../")
-subprocess.call("rm -r *", shell=True)
-
 # source only:
 printinfo("Building QCustomPlot-source package")
 for f in ["../qcustomplot.h", "../qcustomplot.cpp", "../GPL.txt", "../changenotes.txt"]:
   shutil.copy2(f, "./")
+subprocess.call("find . -name .gitignore -exec rm -f \"{}\" \;", shell=True)
 subprocess.call(tarcommand+" QCustomPlot-source"+tarsuffix+" *", shell=True)
 shutil.move("QCustomPlot-source"+tarsuffix, "../")
 subprocess.call("rm -r *", shell=True)
 
-# source and examples:
-printinfo("Building QCustomPlot package")
+# full:
+printinfo("Building full QCustomPlot package")
+distutils.dir_util.copy_tree("../documentation/html", "./documentation/html")
+shutil.copy2("../documentation/qthelp/qcustomplot.qch", "./documentation/")
 for f in ["../qcustomplot.h", "../qcustomplot.cpp", "../GPL.txt", "../changenotes.txt"]:
   shutil.copy2(f, "./")
-distutils.dir_util.copy_tree("../plot-examples", "./plot-examples")
-distutils.dir_util.copy_tree("../interaction-example", "./interaction-example")
-
-os.chdir("./plot-examples");
-subprocess.call("qmake", shell=True)
-subprocess.call("make clean", shell=True)
-for f in ["./plot-examples.pro.user", "./plot-examples", "./Makefile"]:
-  if os.path.isfile(f):
-    os.remove(f)
+distutils.dir_util.copy_tree("../examples", "./examples")
+os.chdir("./examples/plots");
 shutil.rmtree("./screenshots")
-removeExcludeBlockFromFile("plot-examples.pro");
-
-os.chdir("../interaction-example");
-subprocess.call("qmake", shell=True)
-subprocess.call("make clean", shell=True)
-for f in ["./interaction-example.pro.user", "./interaction-example", "./Makefile"]:
-  if os.path.isfile(f):
-    os.remove(f)
-
-os.chdir("../");
+os.chdir("../../");
+subprocess.call("find . -name .gitignore -exec rm -f \"{}\" \;", shell=True)
 subprocess.call(tarcommand+" QCustomPlot"+tarsuffix+" *", shell=True)
 shutil.move("QCustomPlot"+tarsuffix, "../")
 subprocess.call("rm -r *", shell=True)
 
 # shared lib:
 printinfo("Building QCustomPlot-sharedlib package")
-shutil.copy2("../sharedlib/readme.txt", "./");
-os.mkdir("sharedlib");
-shutil.copy2("../sharedlib/sharedlib.pro", "./sharedlib/");
-os.mkdir("sharedlib-demo");
-for f in ["../sharedlib-demo/sharedlib-demo.pro", "../plot-examples/main.cpp", "../plot-examples/mainwindow.h", "../plot-examples/mainwindow.cpp", "../plot-examples/mainwindow.ui"]:
-  shutil.copy2(f, "./sharedlib-demo/")
+distutils.dir_util.copy_tree("../sharedlib/", "./")
+subprocess.call("find . -name .gitignore -exec rm -f \"{}\" \;", shell=True)
 subprocess.call(tarcommand+" QCustomPlot-sharedlib"+tarsuffix+" *", shell=True)
 shutil.move("QCustomPlot-sharedlib"+tarsuffix, "../")
 subprocess.call("rm -r *", shell=True)
