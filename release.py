3063a85bc8bc9f5264b0721317f18f7a3325693a
Emanuel Eichhammer <DerManu@WorksLikeClockwork.com>
2013-05-16 02:46:27 +0200

    - Updated release.py to clean working directory and make full release on its own

--- release.py
+++ release.py
@@ -6,22 +6,45 @@ def printinfo(message):
 
 def printerror(message):
   print "\033[1;31m"+message+"\033[1;m"
+  
+def runQmakeMake(qmakecommand):
+  if subprocess.call(qmakecommand, shell=True) != 0:
+    printerror("qmake failed"); sys.exit(1)
+  if subprocess.call("make -j4", shell=True) != 0:
+    printerror("make failed"); sys.exit(1)
 
 tarcommand = "GZIP=\"-9\" tar -caf" # -a means determine compressor by archive suffix
 tarsuffix = ".tar.gz"
 
+
+if raw_input("\033[1;31m"+"This will call 'git clean -dxf' which will completely reset the current working directory.\n(Call 'git clean -dxn' to see what will be deleted.)\nContinue? (y/n): "+"\033[1;m").lower() != 'y':
+  printinfo("Aborted.");
+  sys.exit(1);
+
 os.chdir(sys.path[0]) # change current working dir to script dir
+
+# clean working dir:
+printinfo("Cleaning working directory...")
+if subprocess.call("git clean -dxf", shell=True) != 0:
+  printerror("Failed to clean working directory with git."); sys.exit(1)
+# amalgamate sources:
+printinfo("Amalgamating sources...")
+subprocess.call("./run-amalgamate.sh", shell=True)
+# generate documentation images:
+printinfo("Generating documentation images...")
+os.chdir("./other/doc-image-generator")
+runQmakeMake("qmake474")
+if subprocess.call("./doc-image-generator", shell=True) != 0:
+  printerror("Failed to generate documentation images."); sys.exit(1)
+os.chdir("../..")
+# generate documentation:
+printinfo("Compiling documentation...")
+subprocess.call("./run-doxygen.sh", shell=True)
+
+# build release packages in temp directory:
+print ""
 os.mkdir("./temp")
 os.chdir("./temp")
-
-# source only:
-printinfo("Building QCustomPlot-source package")
-for f in ["../qcustomplot.h", "../qcustomplot.cpp", "../GPL.txt", "../changenotes.txt"]:
-  shutil.copy2(f, "./")
-subprocess.call("find . -name .gitignore -exec rm -f \"{}\" \;", shell=True)
-subprocess.call(tarcommand+" QCustomPlot-source"+tarsuffix+" *", shell=True)
-shutil.move("QCustomPlot-source"+tarsuffix, "../")
-subprocess.call("rm -r *", shell=True)
 
 # full:
 printinfo("Building full QCustomPlot package")
@@ -36,6 +59,15 @@ os.chdir("../../");
 subprocess.call("find . -name .gitignore -exec rm -f \"{}\" \;", shell=True)
 subprocess.call(tarcommand+" QCustomPlot"+tarsuffix+" *", shell=True)
 shutil.move("QCustomPlot"+tarsuffix, "../")
+subprocess.call("rm -r *", shell=True)
+
+# source only:
+printinfo("Building QCustomPlot-source package")
+for f in ["../qcustomplot.h", "../qcustomplot.cpp", "../GPL.txt", "../changenotes.txt"]:
+  shutil.copy2(f, "./")
+subprocess.call("find . -name .gitignore -exec rm -f \"{}\" \;", shell=True)
+subprocess.call(tarcommand+" QCustomPlot-source"+tarsuffix+" *", shell=True)
+shutil.move("QCustomPlot-source"+tarsuffix, "../")
 subprocess.call("rm -r *", shell=True)
 
 # shared lib:
