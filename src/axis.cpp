c3d3e8816d57e179f21ab89c0a61490194fab32b
Emanuel Eichhammer <DerManu@WorksLikeClockwork.com>
2013-07-11 01:39:24 +0200

    - Added extra pixel to tick label cache pixmap width, to include anti-aliasing that seems to go beyond the QFontMetric's bounding box

--- src/axis.cpp
+++ src/axis.cpp
@@ -2276,7 +2276,7 @@ QCPAxis::TickLabelData QCPAxis::getTickLabelData(const QFont &font, const QStrin
     // calculate bounding rects of base part, exponent part and total one:
     result.baseBounds = QFontMetrics(result.baseFont).boundingRect(0, 0, 0, 0, Qt::TextDontClip, result.basePart);
     result.expBounds = QFontMetrics(result.expFont).boundingRect(0, 0, 0, 0, Qt::TextDontClip, result.expPart);
-    result.totalBounds = result.baseBounds.adjusted(0, 0, result.expBounds.width()+1, 0);
+    result.totalBounds = result.baseBounds.adjusted(0, 0, result.expBounds.width()+2, 0); // +2 consists of the 1 pixel spacing between base and exponent (see drawTickLabel) and an extra pixel to include AA
   } else // useBeautifulPowers == false
   {
     result.basePart = text;
