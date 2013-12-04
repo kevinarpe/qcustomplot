57ebc7a1742825937d09fe92e8ccca268ab7d47c
Emanuel Eichhammer <support@qcustomplot.com>
2013-12-04 15:51:28 +0100

    - Added auto test case for rescaleAxes on multiple flat graphs

--- tests/auto/test-qcustomplot/test-qcustomplot.cpp
+++ tests/auto/test-qcustomplot/test-qcustomplot.cpp
@@ -53,7 +53,21 @@ void TestQCustomPlot::rescaleAxes_FlatGraph()
   QCOMPARE(mPlot->yAxis->range().upper, 0.1);
 }
 
-
+void TestQCustomPlot::rescaleAxes_MultipleFlatGraphs()
+{
+  // test rescaling when there are multiple flat graphs:
+  mPlot->addGraph();
+  mPlot->addGraph();
+  mPlot->graph(0)->setData(QVector<double>()<<1<<2<<3, QVector<double>()<<0<<0<<0);
+  mPlot->graph(1)->setData(QVector<double>()<<-1<<0<<1, QVector<double>()<<2<<2<<2);
+  
+  mPlot->rescaleAxes();
+  
+  QCOMPARE(mPlot->xAxis->range().lower, -1.0);
+  QCOMPARE(mPlot->xAxis->range().upper, 3.0);
+  QCOMPARE(mPlot->yAxis->range().lower, 0.0);
+  QCOMPARE(mPlot->yAxis->range().upper, 2.0);
+}
 
 
 
