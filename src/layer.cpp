kevinarpe/qcustomplot

commit ff342dcd6c6823b0b640e954912777c154a61218

- Added visibility property to layers (influences via QCPLayerable::realVisibility) - Default plot layout now is placed onto layer "main" in QCP ctor

src/layer.cpp
@@ -104,7 +104,8 @@ QCPLayer::QCPLayer(QCustomPlot *parentPlot, const QString &layerName) :
   QObject(parentPlot),
   mParentPlot(parentPlot),
   mName(layerName),
-  mIndex(-1) // will be set to a proper value by the QCustomPlot layer creation function
+  mIndex(-1), // will be set to a proper value by the QCustomPlot layer creation function
+  mVisible(true)
 {
   // Note: no need to make sure layerName is unique, because layer
   // management is done with QCustomPlot functions.
@@ -122,6 +123,19 @@ QCPLayer::~QCPLayer()
   
   if (mParentPlot->currentLayer() == this)
     qDebug() << Q_FUNC_INFO << "The parent plot's mCurrentLayer will be a dangling pointer. Should have been set to a valid layer or 0 beforehand.";
+}
+
+/*!
+  Sets whether this layer is visible or not. If \a visible is set to false, all layerables on this
+  layer will be invisible.
+
+  This function doesn't change the visibility property of the layerables (\ref
+  QCPLayerable::setVisible), but the \ref QCPLayerable::realVisibility of each layerable takes the
+  visibility of the parent layer into account.
+*/
+void QCPLayer::setVisible(bool visible)
+{
+  mVisible = visible;
 }
 
 /*! \internal
@@ -337,9 +351,9 @@ void QCPLayerable::setAntialiased(bool enabled)
 }
 
 /*!
-  Returns whether this layerable is visible, taking possible direct layerable parent visibility
-  into account. This is the method that is consulted to decide whether a layerable shall be drawn
-  or not.
+  Returns whether this layerable is visible, taking the visibility of the layerable parent and the
+  visibility of the layer this layerable is on into account. This is the method that is consulted
+  to decide whether a layerable shall be drawn or not.
   
   If this layerable has a direct layerable parent (usually set via hierarchies implemented in
   subclasses, like in the case of QCPLayoutElement), this function returns true only if this
@@ -351,7 +365,7 @@ void QCPLayerable::setAntialiased(bool enabled)
 */
 bool QCPLayerable::realVisibility() const
 {
-  return mVisible && (!mParentLayerable || mParentLayerable.data()->realVisibility());
+  return mVisible && mLayer && mLayer->visible() && (!mParentLayerable || mParentLayerable.data()->realVisibility());
 }
 
 /*!

src/core.cpp
@@ -574,6 +574,7 @@ QCustomPlot::QCustomPlot(QWidget *parent) :
   mPlotLayout = new QCPLayoutGrid;
   mPlotLayout->initializeParentPlot(this);
   mPlotLayout->setParent(this); // important because if parent is QWidget, QCPLayout::sizeConstraintsChanged will call QWidget::updateGeometry
+  mPlotLayout->setLayer("main");
   QCPAxisRect *defaultAxisRect = new QCPAxisRect(this, true);
   mPlotLayout->addElement(0, 0, defaultAxisRect);
   xAxis = defaultAxisRect->axis(QCPAxis::atBottom);

src/layer.h
@@ -42,6 +42,7 @@ class QCP_LIB_DECL QCPLayer : public QObject
   Q_PROPERTY(QString name READ name)
   Q_PROPERTY(int index READ index)
   Q_PROPERTY(QList<QCPLayerable*> children READ children)
+  Q_PROPERTY(bool visible READ visible WRITE setVisible)
   /// \endcond
 public:
   QCPLayer(QCustomPlot* parentPlot, const QString &layerName);
@@ -52,6 +53,10 @@ public:
   QString name() const { return mName; }
   int index() const { return mIndex; }
   QList<QCPLayerable*> children() const { return mChildren; }
+  bool visible() const { return mVisible; }
+  
+  // setters:
+  void setVisible(bool visible);
   
 protected:
   // property members:
@@ -59,6 +64,7 @@ protected:
   QString mName;
   int mIndex;
   QList<QCPLayerable*> mChildren;
+  bool mVisible;
   
   // non-virtual methods:
   void addChild(QCPLayerable *layerable, bool prepend);

commit f7f4babbb75608a268c83d3a3d9cab28c95725f8

- fixed visibility bug that was introduced in commit ff342dcd6c6   The legend's parent layerable QCPLayoutInset wasn't on any layer and thus invisible. Now, realVisibility   doesn't return false anymore just because the layerable isn't on a layer. This bug only affected layerables   whose parents are in that situation, because layerables that aren't on a layer aren't directly drawn   in QCustomPlot::draw anyway.

src/layer.cpp
@@ -365,7 +365,7 @@ void QCPLayerable::setAntialiased(bool enabled)
 */
 bool QCPLayerable::realVisibility() const
 {
-  return mVisible && mLayer && mLayer->visible() && (!mParentLayerable || mParentLayerable.data()->realVisibility());
+  return mVisible && (!mLayer || mLayer->visible()) && (!mParentLayerable || mParentLayerable.data()->realVisibility());
 }
 
 /*!