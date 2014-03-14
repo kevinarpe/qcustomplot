c5faf5c04308e2b1ef5fd6755eb2e4ce13351370
Emanuel Eichhammer <support@qcustomplot.com>
2014-03-14 01:58:47 +0100

    - Updated Changelog - Bumped version from 1.2.0-beta to 1.2.0

--- sharedlib/sharedlib-compilation/sharedlib-compilation.pro
+++ sharedlib/sharedlib-compilation/sharedlib-compilation.pro
@@ -8,7 +8,7 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets printsupport
 DEFINES += QCUSTOMPLOT_COMPILE_LIBRARY
 TEMPLATE = lib
 CONFIG += shared debug_and_release build_all
-VERSION = 1.2.0-beta
+VERSION = 1.2.0
 
 TARGET = qcustomplot
 CONFIG(debug, debug|release) {
--- src/core.cpp
+++ src/core.cpp
@@ -19,8 +19,8 @@
 ****************************************************************************
 **           Author: Emanuel Eichhammer                                   **
 **  Website/Contact: http://www.qcustomplot.com/                          **
-**             Date: 28.01.14                                             **
-**          Version: 1.2.0-beta                                           **
+**             Date: 14.03.14                                             **
+**          Version: 1.2.0                                                **
 ****************************************************************************/
 
 /*! \file */
@@ -37,7 +37,7 @@
 #include "plottables/plottable-graph.h"
 #include "item.h"
 
-/*! \mainpage %QCustomPlot 1.2.0-beta Documentation
+/*! \mainpage %QCustomPlot 1.2.0 Documentation
 
   \image html qcp-doc-logo.png
   
