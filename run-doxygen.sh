0ee5a8ab5a6090ca57d5d23992275b329b5bcb77
Emanuel Eichhammer <support@qcustomplot.com>
2015-01-02 02:37:14 +0100

    - Doc image palette optimization via postprocess-doc-images.sh to reduce file sizes - Documentation qch is not built by doxygen anymore, to allow for postprocessing step of images - Extended run-doxygen.sh to take care of calling image postprocessing script and generating qch

--- run-doxygen.sh
+++ run-doxygen.sh
@@ -3,7 +3,19 @@ cd "$( dirname "$0" )"
 rm -r documentation/html
 mkdir documentation/html
 
+# let doxygen generate qhp index and html hierarchy from source files:
 doxygen
-cd documentation/html
-rm *.map *.md5 index.qhp
+
+# do post-processing on documentation images in html hierarchy:
+echo Post-processing documentation images...
+sh documentation/postprocess-doc-images.sh
+
+# generate qhc file from qhp index for QtAssistant integration:
+echo Building qhc...
+qhelpgenerator documentation/html/index.qhp -o documentation/qthelp/qcustomplot.qch
+
+# cleanup html hierarchy:
+rm documentation/html/index.qhp
+rm documentation/html/*.map
+rm documentation/html/*.md5
 
